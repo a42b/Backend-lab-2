f5708d8242a115289e6b34b7597a2050bd6da68d
a42b <toniabondar31@gmail.com>
2024-11-06 12:42:39 +0100

    Define project structure

---
+++ models.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+# In-memory storage
+users = {}
+categories = {}
+records = {}
+
+# Example ID counters
+user_id_counter = 1
+category_id_counter = 1
+record_id_counter = 1
