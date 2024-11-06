400a1d05acabf8de2601cd79660497eefe49a4d8
a42b <toniabondar31@gmail.com>
2024-11-06 13:43:04 +0100

    Change host

--- app.py
+++ app.py
@@ -100,5 +100,5 @@ def get_records():
     
     return jsonify(filtered_records)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
