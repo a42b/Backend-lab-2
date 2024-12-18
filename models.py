c091e1773b9df44a167ca5d7d91c4e073a2c29c0
a42b <toniabondar31@gmail.com>
2024-12-19 00:11:20 +0100

    add models

--- models.py
+++ models.py
@@ -1,11 +1,22 @@
 from datetime import datetime
+from extension import db 
 
-# In-memory storage
-users = {}
-categories = {}
-records = {}
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    account = db.relationship('Account', backref='user', uselist=False, cascade="all, delete-orphan")
 
-# Example ID counters
-user_id_counter = 1
-category_id_counter = 1
-record_id_counter = 1
+# Account Model
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=True)
+    balance = db.Column(db.Float, default=0.0, nullable=False)
+
+# Record Model (Expenses)
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
